86d745f118d04df112900b6dd149f3842041a416
Massamba505 <sabelomaphalala131@gmail.com>
2024-07-27 11:33:50 +0200

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/api/hello', function(req, res, next) {
+  res.json({ message: 'Hello from the Express server!' });
+});
+router.get('/', function(req, res, next) {
+  res.sendFile(express.static(path.resolve(__dirname, '../../frontend/dist',"index.html")));
+});
+
+module.exports = router;
