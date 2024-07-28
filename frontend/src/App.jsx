e8866a347703ec03dc4d9e7356a1edd0305dcf40
bidorita47@gmail.com <bidorita47@gmail.com>
2024-07-28 12:38:50 +0200

    video

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,10 @@ import CoursePage from './components/CoursePage/CoursePage';
 import CommunityPage from './components/CommunityPage/CommunityPage';
 import AdminDashboard from './components/AdminDashBoard/AdminDashboard';
 import WelcomePage from './components/WelcomePage/WelcomePage';
-import CourseCard from './components/interactiveHome/interactiveHome';
+import VideoUpload from './components/VideoUpload/VideoUpload';
+import interactiveHome from './components/interactiveHome/interactiveHome';
+import BlobListPage from './components/BlobList/BlobListPage';
+
 import Auth from './components/Auth';
 import { Toaster } from 'react-hot-toast';
 
@@ -20,7 +23,7 @@ const App = () => {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/CourseCard" element={<CourseCard />} />
+        {/* <Route path="/interactiveHome" element={<InteractiveHome />} /> */}
         {/* Protected routes */}
         <Route path="/home" element={<Auth element={<HomePage />} requiresAuth={true} />} />
         <Route path="/live-tutoring" element={<Auth element={<LiveTutoringPage />} requiresAuth={true} />} />
@@ -28,6 +31,7 @@ const App = () => {
         <Route path="/course-page" element={<Auth element={<CoursePage />} requiresAuth={true} />} />
         <Route path="/community-page" element={<Auth element={<CommunityPage />} requiresAuth={true} />} />
         <Route path="/admin-dashboard" element={<Auth element={<AdminDashboard />} requiresAuth={true} />} />
+        <Route path="/video-upload" element={<Auth element={<VideoUpload />} requiresAuth={true} />} />
       </Routes>
       <Toaster />
     </div>
